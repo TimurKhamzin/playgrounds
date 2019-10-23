e72879382ab03cada77b4c797c4c5eef4f659d9d
Timur Khamzin <hamzin.timur0@gmail.com>
2019-10-23 19:48:11 +0300

    New Playgrounds

--- NewPlayground.playground/Contents.swift
+++ NewPlayground.playground/Contents.swift
@@ -1,66 +1,123 @@
-import UIKit
 
-//Создание Класса сотрудник
+/*
+Создать интерфейс Кадровой службы. Регистрируются компании, сотрудники. Сотрудников можно принять и уволить. Данные, необходимые у сотрудника: ФИО, Должность.
+
+Создать класс Компания.
+Поля класса:
+Название
+Сотрудники (Массив класса Сотрудник)
+
+Класс Сотрудник:
+Поля:
+ФИО (кортеж)
+Должность
+ 
+*/
+
+
+//Создаём класс "Работник"
 class Employee {
-    //если мы не задаем значения , то ставим : , если задаем значения то ставим =
-    let fio: (lastName: String, firstName: String, fatherName: String) //это кортеж
-    //создание переменной (константы) позиции сотруника
-    let position: String
+    //Кортеж ФИО
+    let fio: (lastName: String, firstName: String, fatherName: String)
+    //Должность
+    var position: String
+    var bollet: Int
     
-    //Инициализация класса
-    init(fio: ( lastName: String, firstName: String, fatherName: String), position: String) {
+    //Инициализатор класса "Работник"
+    init(fio: (lastName: String, firstName: String, fatherName: String), position: String, bollet: Int) {
         self.fio = fio
         self.position = position
+        self.bollet = bollet
     }
-    
 }
 
-
-
-//Создание Класса Компания
+//Создаём класс "Компания"
 class Company {
-    //Создание переменных (название компании, айди сотрудника, словарь сотрудников в которых есть два значения - айди и имя)
-    let companyName: String
-    var id: Int = 1
-    var emloyeesDictionary = [Int:Employee]()
     
-    //инициализация класса
+    let companyName: String
+    var employeesDict = [Int:Employee]()
+    var id: Int = 1 //Табельный номер сотрудника
+    var sickLeave: Array = [String]() //создание пустого массива , чтобы доавблять сотрудников которые уходят на больничный
+    
     init(companyName: String) {
         self.companyName = companyName
     }
     
-    //Добавление сотрудника
-    func addEmployeeToDictionary(employee: Employee) {
-        emloyeesDictionary [id] = employee
+
+    
+    //Метод, Добавляющий сотрудника в компанию
+    func addEmloyee(employee: Employee) {
+        employeesDict [id] = employee
         id += 1
     }
     
-    
-    //Удаление сотрудника
+    //Метод, увольняющий сотрудника
     func removeEmployee(employee: Int) {
-        guard emloyeesDictionary.keys.contains(employee) else {
+        guard employeesDict.keys.contains(employee) else {
             print("Employee is not found")
             return
         }
-        emloyeesDictionary.removeValue(forKey: employee)
-        print("Employee is delete")
+        employeesDict.removeValue(forKey: employee)
+        print ("Employee is delete")
     }
     
-    //Вывод в консоль оставшихся сотрудников
-    func printToConsoleAllEmployees() {
-        for (key,value) in emloyeesDictionary {
-            print(value.fio.fatherName)
+    // метод отправляющий сотрудника на больничный
+    
+    func addEmployeeToSickLeave(employee: Employee) {
+        if employee.bollet == 1 {
+            print ("\(employee) на больничном")
+        } else {
+            print ("\(employee) работает")
+        }
+    }
+    
+    func printNoSickLeaveEmployees() {
+        print("\(employee.bollet)")
+    }
+    
+    //Метод, печатающий действующих сотрудников компании
+    func printExistedEmployees() {
+        for (_,value) in employeesDict {
+            print(value.fio.lastName)
         }
     }
 }
 
-let company1 = Company(companyName: "IT-Company")
-//print(company1.companyName)
+//Создание экземпляра Компании (конкретной компании)
+let company1 = Company(companyName: "Roga & Kopyta")
 
-var employee1 = Employee(fio: (lastName: "Pypkin", firstName: "Victor", fatherName: "Aleekseevich"), position: "SEO")
+//Создание экземпляра Работника (конкретнго Работника)
+var employee1: Employee = Employee(fio: (lastName: "Pupkin1", firstName: "Victor", fatherName: "Petrovith"), position: "CEO1", bollet: 1)
+var employee2: Employee = Employee(fio: (lastName: "Pupkin2", firstName: "Victor", fatherName: "Petrovith"), position: "CEO2", bollet: 0)
+var employee3: Employee = Employee(fio: (lastName: "Pupkin3", firstName: "Victor", fatherName: "Petrovith"), position: "CEO3", bollet: 1)
+var employee4: Employee = Employee(fio: (lastName: "Pupkin4", firstName: "Victor", fatherName: "Petrovith"), position: "CEO4", bollet: 0)
 
-company1.addEmployeeToDictionary(employee: employee1)
-print(company1.emloyeesDictionary[1]!.fio.firstName)
 
-company1.printToConsoleAllEmployees()
+
+
+//Добавляем работника в компанию
+company1.addEmloyee(employee: employee1)
+company1.addEmloyee(employee: employee2)
+company1.addEmloyee(employee: employee3)
+company1.addEmloyee(employee: employee4)
+
+//Выводим сотрудников , которые не на больничном
+print(company1.employeesDict[1]!.fio.firstName + " " + company1.employeesDict[1]!.fio.lastName)
+
+//Увольняем сотрудника из компании
+company1.removeEmployee(employee: 1)
+
+//Печатаем сотрудников компании
+company1.printExistedEmployees()
+
+company1.printNoSickLeaveEmployees()
+
+
+// ДЗ
+// Создать метод, отправляющий сотрудника на больничный
+// Создать метод, печатающий ФИО сотрудников, которые не на больничном, а также количество.
+
+
+
+
 
